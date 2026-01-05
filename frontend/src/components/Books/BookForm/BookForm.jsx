e11329af0d9fbb041738b9d7d4fafdf845f633d4
Dixneuf Code <dixneufcode@gmail.com>
2026-01-05 11:03:11 +0100

    fix: indentation, simple quote

--- frontend/src/components/Books/BookForm/BookForm.jsx
+++ frontend/src/components/Books/BookForm/BookForm.jsx
@@ -1,24 +1,26 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import React, { useEffect, useMemo, useState } from "react";
-import { useForm } from "react-hook-form";
-import * as PropTypes from "prop-types";
-import { useNavigate } from "react-router-dom";
-import { generateStarsInputs } from "../../../lib/functions";
-import { useFilePreview } from "../../../lib/customHooks";
-import addFileIMG from "../../../images/add_file.png";
-import styles from "./BookForm.module.css";
-import { updateBook, addBook } from "../../../lib/common";
+import React, { useEffect, useMemo, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import * as PropTypes from 'prop-types';
+import { useNavigate } from 'react-router-dom';
+import { generateStarsInputs } from '../../../lib/functions';
+import { useFilePreview } from '../../../lib/customHooks';
+import addFileIMG from '../../../images/add_file.png';
+import styles from './BookForm.module.css';
+import { updateBook, addBook } from '../../../lib/common';
 
 function BookForm({ book, validate }) {
   const userRating = book
-    ? book.ratings.find((elt) => elt.userId === localStorage.getItem("userId"))
-        ?.grade
+    ? book.ratings.find((elt) => elt.userId === localStorage.getItem('userId'))
+      ?.grade
     : 0;
 
   const [rating, setRating] = useState(0);
 
   const navigate = useNavigate();
-  const { register, watch, formState, handleSubmit, reset } = useForm({
+  const {
+    register, watch, formState, handleSubmit, reset,
+  } = useForm({
     defaultValues: useMemo(
       () => ({
         title: book?.title,
@@ -26,13 +28,13 @@ function BookForm({ book, validate }) {
         year: book?.year,
         genre: book?.genre,
       }),
-      [book]
+      [book],
     ),
   });
   useEffect(() => {
     reset(book);
   }, [book]);
-  const file = watch(["file"]);
+  const file = watch(['file']);
   const [filePreview] = useFilePreview(file);
 
   useEffect(() => {
@@ -51,7 +53,7 @@ function BookForm({ book, validate }) {
     // When we create a new book
     if (!book) {
       if (!data.file[0]) {
-        alert("Vous devez ajouter une image");
+        alert('Vous devez ajouter une image');
       }
       if (!data.rating) {
         /* eslint-disable no-param-reassign */
@@ -67,7 +69,7 @@ function BookForm({ book, validate }) {
     } else {
       const updatedBook = await updateBook(data, data.id);
       if (!updatedBook.error) {
-        navigate("/");
+        navigate('/');
       } else {
         alert(updatedBook.message);
       }
@@ -77,54 +79,54 @@ function BookForm({ book, validate }) {
   const readOnlyStars = !!book;
   return (
     <form onSubmit={handleSubmit(onSubmit)} className={styles.Form}>
-      <input type="hidden" id="id" {...register("id")} />
-      <label htmlFor="title">
+      <input type='hidden' id='id' {...register('id')} />
+      <label htmlFor='title'>
         <p>Titre du livre</p>
-        <input type="text" id="title" {...register("title")} />
+        <input type='text' id='title' {...register('title')} />
       </label>
-      <label htmlFor="author">
+      <label htmlFor='author'>
         <p>Auteur</p>
-        <input type="text" id="author" {...register("author")} />
+        <input type='text' id='author' {...register('author')} />
       </label>
-      <label htmlFor="year">
+      <label htmlFor='year'>
         <p>Année de publication</p>
-        <input type="text" id="year" {...register("year")} />
+        <input type='text' id='year' {...register('year')} />
       </label>
-      <label htmlFor="genre">
+      <label htmlFor='genre'>
         <p>Genre</p>
-        <input type="text" id="genre" {...register("genre")} />
+        <input type='text' id='genre' {...register('genre')} />
       </label>
-      <label htmlFor="rate">
+      <label htmlFor='rate'>
         <p>Note</p>
         <div className={styles.Stars}>
           {generateStarsInputs(rating, register, readOnlyStars)}
         </div>
       </label>
-      <label htmlFor="file">
+      <label htmlFor='file'>
         <p>Visuel</p>
         <div className={styles.AddImage}>
-          {filePreview || book?.image ? (
+          {filePreview || book?.imageUrl ? (
             <>
               <img
                 src={filePreview ?? book?.imageUrl ?? addFileIMG}
-                alt={book?.title ?? "preview"}
+                alt={book?.title ?? 'preview'}
               />
               <p>
                 {filePreview || book?.imageUrl
-                  ? "Modifier"
-                  : "Ajouter une image"}
+                  ? 'Modifier'
+                  : 'Ajouter une image'}
               </p>
             </>
           ) : (
             <>
-              <img src={addFileIMG} alt="Add file" />
+              <img src={addFileIMG} alt='Add file' />
               <p>Ajouter une image</p>
             </>
           )}
         </div>
-        <input {...register("file")} type="file" id="file" />
+        <input {...register('file')} type='file' id='file' />
       </label>
-      <button type="submit">Publier</button>
+      <button type='submit'>Publier</button>
     </form>
   );
 }
@@ -143,7 +145,7 @@ BookForm.propTypes = {
       PropTypes.shape({
         userId: PropTypes.string,
         grade: PropTypes.number,
-      })
+      }),
     ),
     averageRating: PropTypes.number,
   }),
